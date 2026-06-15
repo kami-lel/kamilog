4c4ee47871e37abc021f5909bd67d32c34115598
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-06-16 03:50:07 +0800

    Refactor relative_time.py: streamline logging, add propagate
    ~`relative_time.py`

--- examples/relative_time.py
+++ examples/relative_time.py
@@ -3,26 +3,26 @@
 import time
 import kamilog
 
-# --- positive elapsed time (relative_to in the past) ---
+print("# positive elapsed time (relative_to in the past)  #" + "#" * 30)
 start = time.time()
 log = kamilog.getLogger("app", relative_to=start)
 log.setLevel(kamilog.DEBUG)
+log.propagate = False
 
-log.info("started")             # +00:00:00.00x [INFO ] app:      started
+log.info("started")
 time.sleep(0.5)
-log.debug("half a second in")  # +00:00:00.5xx [DEBUG] app:      half a second in
+log.debug("half a second elapsed")
 time.sleep(1.0)
-log.warning("one and a half seconds in")  # +00:00:01.5xx [WARN ] app:      one and a half seconds in
+log.warning("one and a half seconds elapsed")
 
-print()
-
-# --- negative elapsed time (relative_to in the future) ---
+print("\n# negative elapsed time (relative_to in the future)  #" + "#" * 28)
 future = time.time() + 2.0
 log2 = kamilog.getLogger("countdown", relative_to=future)
 log2.setLevel(kamilog.DEBUG)
+log2.propagate = False
 
-log2.info("two seconds before reference")   # -00:00:02.xxx [INFO ] countdown: two seconds before reference
+log2.info("two seconds before reference")
 time.sleep(1.0)
-log2.info("one second before reference")    # -00:00:01.xxx [INFO ] countdown: one second before reference
+log2.info("one second before reference")
 time.sleep(1.5)
-log2.info("half a second after reference")  # +00:00:00.5xx [INFO ] countdown: half a second after reference
+log2.info("half a second after reference")
