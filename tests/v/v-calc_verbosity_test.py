5e7340e05de6a295032e0c8e479c4dea003bd252
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-17 00:39:07 +0800

    refactor verbosity API; add calc_verbosity coverage
    #`v-calc_logging_level_namespace_test.py` update test to use public calc_logging_level(namespace) API
    /`v-calc_logging_level_test.py` update tests to use *calc_logging_level* API instead of private helper
    ^`v-calc_verbosity_test.py` add tests for kamilog calc_verbosity flag parsing and offsets

--- tests/v/v-calc_verbosity_test.py
+++ tests/v/v-calc_verbosity_test.py
@@ -1,46 +1,46 @@
 """
-v-calc_logging_level_namespace_test.py
+v-calc_verbosity_test.py
 
-tests for `calc_logging_level`'s `namespace` param in `kamilog.py`
+tests for `calc_verbosity` in `kamilog.py`
 """
 
-import logging
 from argparse import ArgumentParser
-from kamilog.kamilog import (
-    add_verbose_arguments,
-    calc_logging_level,
-    ENTER,
-    DONE,
-)
+from kamilog.kamilog import add_verbose_arguments, calc_verbosity
 
 
-class TestCalcLoggingLevelNamespace:
+class TestCalcVerbosity:
     def test_no_flags(_):
         parser = ArgumentParser()
         add_verbose_arguments(parser)
         args = parser.parse_args([])
-        assert calc_logging_level(0, namespace=args) == DONE
+        assert calc_verbosity(args) == 0
 
     def test_v1(_):
         parser = ArgumentParser()
         add_verbose_arguments(parser)
         args = parser.parse_args(["-v"])
-        assert calc_logging_level(0, namespace=args) == logging.INFO
+        assert calc_verbosity(args) == 1
 
     def test_v2(_):
         parser = ArgumentParser()
         add_verbose_arguments(parser)
         args = parser.parse_args(["-vv"])
-        assert calc_logging_level(0, namespace=args) == ENTER
+        assert calc_verbosity(args) == 2
 
     def test_combined_vv_verbose_qq_net_v1(_):
         parser = ArgumentParser()
         add_verbose_arguments(parser)
         args = parser.parse_args(["-vv", "--verbose", "-qq"])
-        assert calc_logging_level(0, namespace=args) == logging.INFO
+        assert calc_verbosity(args) == 1
 
     def test_quiet(_):
         parser = ArgumentParser()
         add_verbose_arguments(parser)
         args = parser.parse_args(["--quiet"])
-        assert calc_logging_level(0, namespace=args) == logging.WARNING
+        assert calc_verbosity(args) == -1
+
+    def test_base_verbosity_offset(_):
+        parser = ArgumentParser()
+        add_verbose_arguments(parser)
+        args = parser.parse_args(["-v"])
+        assert calc_verbosity(args, verbosity=2) == 3
